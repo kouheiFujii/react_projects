75fbd3e786cdd7d9ac0d15ec6c37bfc5d94d2baa
kouheifujii <kohei.fujii@i-nations.tokyo>
2022-03-01 23:11:21 +0900

    fix: React component version

--- monsters-rolodex/src/App.tsx
+++ monsters-rolodex/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {}
+// Component ver
+class App extends React.Component<{}, { name: string }> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      name: "Alisa",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <p>Hi {this.state.name}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
