8fbe778fae0adf6e25a7a7bd2560f9a20445639f
kouheifujii <kohei.fujii@i-nations.tokyo>
2022-03-08 23:00:36 +0900

    search box

--- monsters-rolodex/src/App.tsx
+++ monsters-rolodex/src/App.tsx
@@ -1,36 +1,52 @@
 import React from "react";
 import { Monsters } from "./interface";
 import "./App.css";
+import { Monster } from "./interface/monsters";
 
 interface Props {}
 class App extends React.Component<{}, Monsters> {
   constructor(props: Props) {
     super(props);
+    console.log("constructor");
+
     this.state = {
-      monsters: [
-        {
-          id: 1,
-          name: "Linda",
-        },
-        {
-          id: 2,
-          name: "Flank",
-        },
-        {
-          id: 3,
-          name: "Jacky",
-        },
-        {
-          id: 4,
-          name: "Andrei",
-        },
-      ],
+      monsters: [],
     };
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((result) => result.json())
+      .then((users: Monster[]) =>
+        this.setState(
+          (): Monsters => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
+    console.log("render");
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monster"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            const searchString = e.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) =>
+              monster.name.toLocaleLowerCase().includes(searchString)
+            );
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>;
         })}
